f10b28d1beed244d89be1e4888cb0195e209242a
Tural Quluzadə <99786998+Guluz4deh@users.noreply.github.com>
2022-03-24 06:06:58 +0400

    Add files via upload

---
+++ 24-03-2022/24-03-2022/Extensions/Extensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _24_03_2022.Extensions
+{
+    public static class Extensions
+    {
+        public static string CapitalizeString(this string text)
+        {
+            char firstLetter = Char.ToUpper(text[0]);
+            StringBuilder stringBuilder = new StringBuilder(text);
+            stringBuilder[0] = firstLetter;
+            text = stringBuilder.ToString();
+            return text;
+        }
+
+
+    }
+}
---
+++ 24-03-2022/24-03-2022/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Text;
+using _24_03_2022.Extensions;
+
+namespace _24_03_2022
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please Enter the Word:");
+            string name = Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine($"Word : {name}");
+        menu:
+            Console.WriteLine("===================================\n1.Capitalize Method.\n2.Reverse Method.\n3.Parameter Receiver Method.\n4.Array adding Method.\n===================================");
+            int menu = Convert.ToInt32(Console.ReadLine());
+            switch (menu)
+            {
+                case 1:
+                    Console.Clear();
+                    Console.WriteLine(name.CapitalizeString());
+                    goto menu;
+                case 2:
+                    Console.Clear();
+                    StringReverse(ref name);
+                    Console.WriteLine(name);
+                    goto menu;
+                case 3:
+                    Console.Clear();
+                    string letterinword = " ";
+                    ParameterReceiver(name, Convert.ToInt32(Console.ReadLine()), ref letterinword);
+                    Console.WriteLine(letterinword);
+                    goto menu;
+                case 4:
+                    Console.Clear();
+
+                    goto menu;
+              
+                default:
+                    Console.Clear();
+                    Console.WriteLine("===Thanks===");
+                    break;
+            }
+
+            //Array arr = new string[3] { "Tural", "Nizami", "Hesen" };
+            //Array.Resize(ref arr, arr.Length + 1);
+            //ArrayAdd(arr[], Console.ReadLine());
+            //foreach (var item in arr)
+            //{
+            //    Console.WriteLine(arr);
+            //}
+
+          
+        }
+        
+        public static void CapitalizeString(ref string text)
+        {
+            char firstletter = Char.ToUpper(text[0]);
+            StringBuilder stringBuilder = new StringBuilder(text);
+            stringBuilder[0] = firstletter;
+            text = stringBuilder.ToString();
+        }
+
+        public static void StringReverse(ref string text)
+        {
+            int j = 0;
+            StringBuilder stringBuilder = new StringBuilder(text);
+            for (int i = text.Length - 1; i >= 0; i--)
+            {
+                stringBuilder[j] = text[i];
+                j++;
+            }
+
+            text = stringBuilder.ToString();
+        }
+
+        public static void ParameterReceiver(string text, int index, ref string letterinword)
+        {
+            char letter = text[index];
+            StringBuilder stringBuilder = new StringBuilder(letterinword);
+            stringBuilder[0] = letter; 
+            letterinword = stringBuilder.ToString();
+        }
+
+        public static void ArrayAdd(ref string[] arr, string addword)
+        {
+            Array.Resize(ref arr, arr.Length + 1);
+            arr[arr.Length - 1] = addword;
+        }
+    }
+}
